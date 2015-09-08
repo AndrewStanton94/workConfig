29c70c4cffa04e89595691f3e1e0f993762e103d
andrewstanton94 <up683693@myport.ac.uk>
2015-09-08 09:55:09 +0100

    Latest version of startUP. After The Discovery of The Second Hard Drive
    Change-Id: I78623636de4786e83d1f3148825bcb72dfbbd223

--- Desktop/startUp.bat
+++ Desktop/startUp.bat
@@ -1,7 +1,6 @@
-start "" "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe" --comment "NetworkGuardianAMD64" --startvm "c9598931-04fd-4e61-abbc-50aec8a88cbb"
+start "" "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe" --comment "Guardain_EG" --startvm "267c96cb-674a-49c2-a94b-2645bf80f76c"
 start "" "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe" --comment "kubuntu" --startvm "ff7c4bde-73c8-431f-b71a-d2e77d5da47d"
-/* start "" "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe" --comment "kubuntu Clone" --startvm "81e66b7d-26dd-4ab9-87f3-c67a12ce3bcf" */
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "https://mail.google.com/mail/u/0/#inbox" "https://www.google.com/calendar/render?tab=mc#main_7" "https://docs.google.com/spreadsheets/d/16rpGdHdR-XPHba5kbNoWtSOQIZjXiJEr_FOIbl9HhLU/edit#gid=0" "https://www.clicktime.com/?home=true"
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --no-proxy-server
 start "" "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
 start "" "C:\Program Files (x86)\Vim\vim74\gvim.exe"
 start "" "C:\Program Files\HexChat\hexchat.exe"
