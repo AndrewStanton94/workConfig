8bc51b303751a436f4ae6d16ef908997b85fec1c
Andrew Stanton <up683693@myport.ac.uk>
2015-10-25 03:21:13 +0000

    Now able to handle 0, 1 & n session cases

--- ta.zsh
+++ ta.zsh
@@ -1,15 +1,27 @@
 ta() {
     sessions=$(tmux list-sessions)
+    num=$(echo "$sessions"| wc -l)
     echo "$sessions"
-    echo "$sessions"| wc -l
-    if (echo "$sessions"| wc -l)>0
+    # echo "$num"
+    if [ "$sessions" = "" ];
     then
-        echo "more than 0"
-        read -p "Choose session: " selectedSession
-        echo "$selectedSession"
-        tmux a -t $selectedSession
+        read -p "Creating first session, name: " sessionName
+        tmux new -s $sessionName
     else
-        echo "less than 0"
+        if [ "$num" -gt 1 ]
+        then
+            # echo "more than 1"
+            read -p "Choose session: " selectedSession
+            echo "$selectedSession"
+            tmux a -t $selectedSession
+            # select s in $sessions
+            # do
+            #     echo "$s chosen"
+            # done
+        else
+            # echo "One session"
+            tmux a
+        fi
     fi
 }
 
