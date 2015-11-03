03129067fdc4222012d704c1f6a4e04cb985d72a
AndrewStanton94 <andrew.stanton@smoothwall.net>
2015-11-03 17:49:10 +0000

    stuff

--- bin/tmux-plugins.sh
+++ bin/tmux-plugins.sh
@@ -1,2 +1,3 @@
 git clone https://github.com/tmux-plugins/tmux-battery
 git clone https://github.com/tmux-plugins/tmux-online-status
+git clone https://github.com/tmux-plugins/tmux-prefix-highlight.git
