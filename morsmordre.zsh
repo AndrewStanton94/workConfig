f2ba00f86f667d000f943739333644be5c998edb
andrew stanton <andrew.stanton>
2015-12-18 15:19:13 +0000

    Changed zsh colourscheme, weechat layout and morsmordre flow

--- morsmordre.zsh
+++ morsmordre.zsh
@@ -2,20 +2,21 @@
 # Need to source the file. Not run.
 # . ./morsmordre.zsh
 export WORKCONFIG="$HOME/Downloads/workconfig"
-
 morsmordre(){
 	cp -v ~/.zshrc $WORKCONFIG
 	cp -v ~/.vimrc $WORKCONFIG
 	cp -v ~/.tmux.conf $WORKCONFIG
 	cp -v ~/morsmordre.zsh $WORKCONFIG
+	cp -v ~/installlist.txt $WORKCONFIG
 
 	cp -v ~/.weechat/weechat.conf "$WORKCONFIG/.weechat/"
 	cp -v ~/.weechat/irc.conf "$WORKCONFIG/.weechat/"
 
 	cd "$WORKCONFIG"
-	git add -A
+	git diff
 	git status
 	echo "Commit message: "
 	read commitMessage
-	git commit -m "$commitMessage"
+	git commit -am "$commitMessage"
+	git push
 }
