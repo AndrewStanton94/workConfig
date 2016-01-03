383f5e0bfd41b8c4eb8a14ce6728b223dd37b81d
Andrew Stanton <up683693@myport.ac.uk>
2016-01-03 15:06:40 +0000

    Restructured clonise, more feedback

--- materialize.sh
+++ materialize.sh
@@ -2,20 +2,13 @@
 export WORKCONFIG="$HOME/Downloads/workConfig"
 
 getFromApt() {
+	echo "Installing from the apt repos"
+	echo "$WORKCONFIG/getFrom/debReb.txt"
 	sudo apt install `cat $WORKCONFIG/getFrom/debReb.txt`
 }
 
-getFromGit() {
-	# Vundle
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-	# Patched Fonts
-	git clone https://github.com/powerline/fonts.git ~/fonts
-	~/fonts/install.sh
-	# oh-my-zsh
-	sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-}
-
 unpackConfig() {
+	echo "Copying config files to home"
 	cp -vu "$WORKCONFIG/.zshrc" ~
 	cp -vu "$WORKCONFIG/.vimrc" ~
 	cp -vu "$WORKCONFIG/.tmux.conf" ~
@@ -36,10 +29,23 @@ materialize(){
 
 colonize(){
 	echo "First time set up"
-	getFromApt
-	unpackConfig
-	getFromGit
+	echo "Git configuring"
 	git config --global push.default simple
 	git config --global user.email up683693@myport.ac.uk
 	git config --global user.name "Andrew Stanton"
+
+	getFromApt
+
+	unpackConfig
+
+	echo "Getting code from github"
+	# Vundle
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	# Patched Fonts
+	git clone https://github.com/powerline/fonts.git ~/fonts
+	~/fonts/install.sh
+
+	echo "About to install ZSH. Once that is done you will need to:\n\tCopy .zshrc from workConfig\n\tConfigure the terminal to use powerline patched font\n\tOpen a new terminal to see it working"
+	# oh-my-zsh
+	sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 }
